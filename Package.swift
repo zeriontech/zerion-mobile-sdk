f94efe88dbe4b0db5473d4f143cd25acce950286
UladzislauB <uladzislau@zerion.io>
2025-03-18 13:38:17 +0100

    correct version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZerionMobileSDK",
-            url: "https://github.com/zeriontech/zerion-mobile-sdk/releases/download/0.0.1/ZerionMobileSDK.xcframework.zip",
+            url: "https://github.com/zeriontech/zerion-mobile-sdk/releases/download/v0.0.1/ZerionMobileSDK.xcframework.zip",
             checksum:"78a89c7b8c77041485854894932a4930a82a9dcd69b1e03f1861f7119b395938"
         )
     ]
